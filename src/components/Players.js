070ebd0b88518f09cb8fe78429c60ec614a67872
absy <ndeyeabsa.fall18@unchk.edu.sn>
2024-06-26 18:05:53 +0200

    first commit

---
+++ src/components/Players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami",
+      nationality: "Argentinian",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvjY7ClFbCGuHOBUwYl0iHuJromSIBXWPC2A&s",
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al Nassr",
+      nationality: "Portuguese",
+      jerseyNumber: 7,
+      age: 39,
+      imageUrl: "https://media.arcinfo.ch/media/image/18/nf_medium_16_9/222625990.jpg",
+    },
+    {
+      name: "Neymar Jr",
+      team: "Al Hilal",
+      nationality: "Brazilian",
+      jerseyNumber: 10,
+      age: 32,
+      imageUrl: "https://media.culturepsg.com/image/news/neymar_puce_leve_conf_04082017.jpg",
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "French",
+      jerseyNumber: 7,
+      age: 25,
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_qrS-L3PEkXc0wl-sYKkahQv6GWPJBnu67g&s",
+    },
+  ];
+  
+  export default players;
+  
